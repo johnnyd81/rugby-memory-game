1d0df54b730be34226faf228b4e93ed7b55ac85c
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-03-21 17:14:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//array of different player card objects i.e. names and images of the pairs of cards used in the game
+//array of different player card objects i.e. names and images of the pairs of cards used in the game ui
 let cardsArray = [
   {
     name: "player1",
